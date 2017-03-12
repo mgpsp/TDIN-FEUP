0d260de7337c750d7e4ec0f9b908ade8b7a0d875
Miguel Pereira <miguelgpspereira@gmail.com>
2017-03-12 18:18:43 +0000

    Connecting with database, inital login form

---
+++ WindowsFormsApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;" +
+                "pwd=tdin2017;database=tdin;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                string sql = " SELECT * FROM user  ";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                conn.Open();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString("username"));
+                    Console.WriteLine(reader.GetString("password"));
+                }
+                Console.Read();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Erro");
+            }
+        }
+    }
+}
