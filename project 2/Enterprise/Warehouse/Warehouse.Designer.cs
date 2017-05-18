07603f55ad153ddcb60387a6d14a25d2f588ec6d
Miguel Pereira <miguelgpspereira@gmail.com>
2017-05-18 18:36:13 +0100

    web client initial

--- project 2/Enterprise/Warehouse/Warehouse.Designer.cs
+++ project 2/Enterprise/Warehouse/Warehouse.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class Form1
+    partial class Warehouse
     {
         /// <summary>
         /// Required designer variable.
--- project 2/Enterprise/Warehouse/Warehouse.cs
+++ project 2/Enterprise/Warehouse/Warehouse.cs
@@ -4,17 +4,26 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class Form1 : Form
+    public partial class Warehouse : Form
     {
-        public Form1()
+        public Warehouse()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var ws = new WebSocket("ws://localhost:9000/socket.io/?EIO=2&transport=websocket"))
+            {
+
+            }
         }
     }
 }
